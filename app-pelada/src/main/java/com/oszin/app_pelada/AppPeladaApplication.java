c4a24d2f8a353ab04dc1bc35f5c48cb2fdfc6219
lucas <lucassr614@gmail.com>
2025-02-16 13:28:10 -0300

    init backend

---
+++ app-pelada/src/main/java/com/oszin/app_pelada/AppPeladaApplication.java
@@ -0,0 +1,12 @@
+package com.oszin.app_pelada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPeladaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPeladaApplication.class, args);
+	}
+}
---
+++ app-pelada/src/test/java/com/oszin/app_pelada/AppPeladaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oszin.app_pelada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPeladaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
